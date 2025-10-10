003133736b5d7bf4c609f4f2f674fd482410ba89
Vic <waleria.tuszkowa@gmail.com>
2025-10-10 21:09:40 +0200

    token

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { Note } from '../types/note';
 
 const API_URL = 'https://notehub-public.goit.study/api';
-const TOKEN = import.meta.env.VITE_API_TOKEN;
+const TOKEN = import.meta.env.VITE_NOTEHUB_TOKEN;
 
 const api = axios.create({
   baseURL: API_URL,
